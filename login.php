551a68343647d226d5f56dba9444deac8b471e15
VipulAgrawal21 <169054960+VipulAgrawal21@users.noreply.github.com>
2024-09-10 11:28:42 +0530

    Update login.php

--- login.php
+++ login.php
@@ -33,7 +33,7 @@ if ( isset($_POST['who']) && isset($_POST['pass']) ) {
 <html>
 <head>
 <?php require_once "bootstrap.php"; ?>
-<title>Meng Shi Severance's Login Page</title>
+<title>Vipul Agrawal's Login Page</title>
 </head>
 <body>
 <div class="container">
