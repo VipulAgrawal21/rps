bee9b0b63771433119224e62da3cd1e7c656bb7b
VipulAgrawal21 <169054960+VipulAgrawal21@users.noreply.github.com>
2024-09-10 11:29:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Vipul Agrawal - Broken Rock Paper Scissors</title>
+<title>Vipul Agrawal - Broken Rock Paper Scissors - af97a874 </title>
 <?php require_once "bootstrap.php"; ?>
 </head>
 <body>
